59d2ce3948f12422657ea41dac98cbc9651d3450
zzyyxxww <guidotarsia@gmail.com>
2019-08-09 19:06:05 -0300

    Add jumping

--- project/scenes/player/Player.gd
+++ project/scenes/player/Player.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 class_name Player
 
-const ACC = 4
-const SPEED = 20
+const ACC = 8
+const SPEED = 40
 const FRIC = 1
-const GRAV = 30
+const GRAV = 160
+
+const JUMP_SPEED = 80
 
 var motion = Vector2()
 
 func _physics_process(delta: float) -> void:
 	handle_gravity(delta)
-	move_and_slide(motion)
+	move_and_slide(motion, Vector2(0, -1))
+	handle_jump()
 	movement_logic()
 	#air_logic()
 	sprite_dir()
@@ -23,17 +26,17 @@ func movement_logic():
 	else:
 		motion.x = lerp(motion.x, 0, FRIC) 
 
-func air_logic():
+func handle_gravity(delta: float):
+	motion.y += delta * GRAV
+
+func handle_jump():
 	if !is_on_floor():
-		motion.y = GRAV
-	else:
-		motion.y = 0
+		return
+	if Input.is_action_pressed("jump"):
+		motion.y = -JUMP_SPEED
 
 func sprite_dir():
 	if motion.x > 0:
 		$Sprite.flip_h = false
 	if motion.x < 0:
 		$Sprite.flip_h = true
-
-func handle_gravity(delta: float):
-	motion.y += delta * GRAV
