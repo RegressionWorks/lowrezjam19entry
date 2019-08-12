51f48b4d2b5e134eb312e22c344bd8b228f25450
zzyyxxww <guidotarsia@gmail.com>
2019-08-12 16:25:07 -0300

    Add wall jumping

--- project/scenes/player/Player.gd
+++ project/scenes/player/Player.gd
@@ -6,8 +6,13 @@ const SPEED = 40
 const FRIC = 1
 const GRAV = 160
 const JUMP_SPEED = 80
+const WALL_GRAB_GRAVITY = 20
+const WALL_JUMP_FRIC = 0.065
+const WALL_JUMP_YSPEED = 70
+const WALL_JUMP_XSPEED = 100
+const WALL_JUMP_SPEED_CUTOFF = 50
 
-var motion = Vector2()
+var motion: Vector2 = Vector2()
 var priority_anims: Array = Array()
 
 func _ready():
@@ -18,6 +23,8 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	movement_logic()
+	handle_wall_grab()
+	handle_wall_jump(delta)
 	handle_gravity(delta)
 	handle_jump()
 	handle_landed()
@@ -27,6 +34,8 @@ func _physics_process(delta: float) -> void:
 	sprite_dir()
 
 func movement_logic():
+	if wall_jumping:
+		return
 	if Input.is_action_pressed("ui_right") && !Input.is_action_pressed("ui_left"):
 		motion.x = clamp((motion.x + ACC), 0, SPEED)
 	elif Input.is_action_pressed("ui_left") && !Input.is_action_pressed("ui_right"):
@@ -34,8 +43,47 @@ func movement_logic():
 	else:
 		motion.x = lerp(motion.x, 0, FRIC) 
 
+var wall_grabbing = false
+var wall_snapping = false
+var wall_jumping = false
+
+func handle_wall_grab():
+	var wall_grab_before = wall_grabbing
+	wall_grabbing = !on_floor() and has_wall()
+	if !wall_grabbing:
+		wall_snapping = false
+	if !wall_grab_before and wall_grabbing:
+		wall_snapping = true
+	if wall_snapping:
+		motion.y = 0
+	if !wall_snapping and wall_grabbing:
+		motion.y = WALL_GRAB_GRAVITY
+	if wall_grabbing and Input.is_action_pressed("jump"):
+		wall_jumping = true
+		motion.x = WALL_JUMP_XSPEED if has_wall_left() else -WALL_JUMP_XSPEED
+		motion.y = -WALL_JUMP_YSPEED
+
+func handle_wall_jump(delta):
+	if wall_jumping:
+		motion.x = lerp(motion.x, 0, WALL_JUMP_FRIC)
+		if abs(motion.x) < WALL_JUMP_SPEED_CUTOFF:
+			wall_jumping = false
+
+func has_wall():
+	return has_wall_left() or has_wall_right()
+
+func has_wall_left():
+	return (
+		$WallLeftUp.is_colliding() or
+		$WallLeftDown.is_colliding())
+	
+func has_wall_right():
+	return ($WallRightUp.is_colliding() or
+		$WallRightDown.is_colliding())
+
+
 func handle_gravity(delta: float):
-	if !on_floor():
+	if !on_floor() and !wall_grabbing:
 		motion.y += delta * GRAV
 
 var jumping = false
@@ -69,23 +117,26 @@ func gun_shooting():
 		fire.position = muzzle.global_position
 
 func set_animations():
-	var current = $Sprite/AnimationPlayer.current_animation
+	var anim_player: AnimationPlayer = $Sprite/AnimationPlayer
+	var current = anim_player.current_animation
 	var is_priority = priority_anims.has(current)
-	var is_playing = $Sprite/AnimationPlayer.is_playing()
+	var is_playing = anim_player.is_playing()
 	var is_priority_playing = is_priority and is_playing
-	if !on_floor() and motion.y > 2:
-		$Sprite/AnimationPlayer.play("fall")
+	if wall_snapping:
+		anim_player.play("wall_snap")
+	if !on_floor() and !wall_snapping and motion.y > 2:
+		anim_player.play("fall")
 	if landed:
-		$Sprite/AnimationPlayer.play("land")
-	if jumping:
-		$Sprite/AnimationPlayer.play("jump")
+		anim_player.play("land")
 	if shooting:
-		$Sprite/AnimationPlayer.play("shoot")
+		anim_player.play("shoot")
+	if jumping or wall_jumping:
+		anim_player.play("jump")
 	elif !is_priority_playing:
 		if on_floor() and motion.x != 0:
-			$Sprite/AnimationPlayer.play("run")
+			anim_player.play("run")
 		elif on_floor() and motion.x == 0:
-			$Sprite/AnimationPlayer.play("idle")
+			anim_player.play("idle")
 
 func sprite_dir():
 	if motion.x > 0:
@@ -95,3 +146,8 @@ func sprite_dir():
 
 func on_floor():
 	return $FloorLeft.is_colliding() or $FloorRight.is_colliding()
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "wall_snap":
+		wall_snapping = false
