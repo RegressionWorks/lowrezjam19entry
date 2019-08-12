fe3633b330d0753e2b47819a0219546d75355049
zzyyxxww <guidotarsia@gmail.com>
2019-08-12 08:02:05 -0300

    Fix idle, run, jump, land and fall animations

--- project/scenes/player/Player.gd
+++ project/scenes/player/Player.gd
@@ -5,15 +5,22 @@ const ACC = 8
 const SPEED = 40
 const FRIC = 1
 const GRAV = 160
-
 const JUMP_SPEED = 80
 
 var motion = Vector2()
+var priority_anims: Array = Array()
+
+func _ready():
+	for anim in $Sprite/AnimationPlayer.get_animation_list():
+		if anim == "run" or anim == "idle":
+			continue
+		priority_anims.append(anim)
 
 func _physics_process(delta: float) -> void:
 	movement_logic()
 	handle_gravity(delta)
 	handle_jump()
+	handle_landed()
 	motion = move_and_slide(motion, Vector2(0, -1))
 	set_animations()
 	sprite_dir()
@@ -27,23 +34,47 @@ func movement_logic():
 		motion.x = lerp(motion.x, 0, FRIC) 
 
 func handle_gravity(delta: float):
-	motion.y += delta * GRAV
+	if !on_floor():
+		motion.y += delta * GRAV
 
+var jumping = false
 func handle_jump():
-	if !is_on_floor():
+	jumping = false
+	if !on_floor():
 		return
 	if Input.is_action_pressed("jump"):
 		motion.y = -JUMP_SPEED
-		$Sprite/AnimationPlayer.play("jump")
+		jumping = true
+
+var falling = true
+var landed = false
+func handle_landed():
+	# landed will be true on a single frame
+	landed = falling and on_floor()
+	falling = !on_floor() && motion.y > 10
 
 func set_animations():
-	if !is_on_floor() and motion.y > 0:
+	var current = $Sprite/AnimationPlayer.current_animation
+	var is_priority = priority_anims.has(current)
+	var is_playing = $Sprite/AnimationPlayer.is_playing()
+	var is_priority_playing = is_priority and is_playing
+	if !on_floor() and motion.y > 2:
 		$Sprite/AnimationPlayer.play("fall")
-	elif motion.x == 0:
-		$Sprite/AnimationPlayer.play("idle")
+	if landed:
+		$Sprite/AnimationPlayer.play("land")
+	if jumping:
+		$Sprite/AnimationPlayer.play("jump")
+	elif !is_priority_playing:
+		if on_floor() and motion.x != 0:
+			$Sprite/AnimationPlayer.play("run")
+		elif on_floor() and motion.x == 0:
+			$Sprite/AnimationPlayer.play("idle")
 
 func sprite_dir():
 	if motion.x > 0:
 		$Sprite.flip_h = false
 	if motion.x < 0:
 		$Sprite.flip_h = true
+
+func on_floor():
+	return $FloorLeft.is_colliding() or $FloorRight.is_colliding()
